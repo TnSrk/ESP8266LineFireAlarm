ea941fb584709b0b1691b7a5e4ce4639abc19de1
TnSrk <tanawut.cool@gmail.com>
2024-06-12 22:28:18 +0700

    Add files via upload

---
+++ include/secret.h
@@ -0,0 +1,10 @@
+#ifndef SECRET
+#define SECRET
+
+const char* ssid = "XXXXXXXX"; // Your Wifi access point name
+const char* password = "XXXXXXXX"; // Your Wifi Password
+const char* mqtt_server = "192.168.1.XXX"; // Your MQTT_server
+const char* LINE_NOTIFY_URL = "https://notify-api.line.me/api/notify"; 
+const char* LINE_TOKEN = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"; // Change to your Line Notify Token
+
+#endif //SECRET
